2b7aff2c49bf46a09577d3208709ab9888e930eb
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-16 18:56:27 +1000

    Add method chaining

--- sandbox.js
+++ sandbox.js
@@ -7,13 +7,16 @@ class User {
     }
     login(){
         console.log(`${this.username} just logged in`);
+        return this;
     }
     logout(){
         console.log(`${this.username} just logged out`);
+        return this;
     }
     incScore(){
         this.score += 1;
         console.log(`${this.username} has a score of ${this.score}`);
+        return this;
     }
 }
 
@@ -29,3 +32,6 @@ userTwo.logout();
 
 userOne.incScore();
 userOne.incScore();
+
+userOne.login().logout();
+userTwo.incScore().incScore().incScore();
