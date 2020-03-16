95c8ff6e5eda68339358943bd5bacdf30b44ac39
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-16 18:55:43 +1000

    Add class methods

--- sandbox.js
+++ sandbox.js
@@ -3,16 +3,29 @@ class User {
         // set up properties
         this.username = username;
         this.email = email;
+        this.score = 0;
+    }
+    login(){
+        console.log(`${this.username} just logged in`);
+    }
+    logout(){
+        console.log(`${this.username} just logged out`);
+    }
+    incScore(){
+        this.score += 1;
+        console.log(`${this.username} has a score of ${this.score}`);
     }
 }
 
 const userOne = new User('mario', 'mario@xxx.com');
 const userTwo = new User('luigi', 'luigi@xxx.com');
 
-console.log(userOne);
-console.log(userTwo);
+console.log(userOne, userTwo);
+userOne.login();
+userTwo.login();
 
-// the 'new' keyword
-// 1 - it creates a new empty object {}
-// 2 - it binds the value of 'this' to the new empty object
-// 3 - it calls the constructor function to 'build' the object
+userOne.logout();
+userTwo.logout();
+
+userOne.incScore();
+userOne.incScore();
