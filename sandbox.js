a1280987eda7af95e6bb4743ec036d790d94232a
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-16 19:38:43 +1000

    Add prototype model methods

--- sandbox.js
+++ sandbox.js
@@ -2,29 +2,22 @@
 function User(username, email){
     this.username = username;
     this.email = email;
-    this.login = function(){
-        console.log(`${this.username} has logged in`);
-        return this;
-    };
-    this.logout = function(){
-        console.log(`${this.username} has logged out`);
-        return this;
-    };
 }
 
-// class User {
-//     constructor(username, email){
-//         // set up properties
-//         this.username = username;
-//         this.email = email;
-//     }
-// }
+User.prototype.login = function(){
+    console.log(`${this.username} has logged in`);
+    return this;
+}
+
+User.prototype.logout = function(){
+    console.log(`${this.username} has logged out`);
+    return this;
+}
 
 const userOne = new User('mario', 'mario@xxx.com');
 const userTwo = new User('luigi', 'luigi@xxx.com');
 console.log(userOne, userTwo);
 
-userOne.login();
 userTwo.login().logout();
 
 // the 'new' keyword
